e55491f9266533dc645a49d7ee61cae439d2b07c
Sgeary88 <97266345+Sgeary88@users.noreply.github.com>
2023-01-15 09:32:16 -0800

    starting to work on contact form submission

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let tabLinks = document.getElementsByClassName('tab-links');
 let tabContents = document.getElementsByClassName('tab-contents');
+let sideMenu = document.getElementById('side-menu');
 
 function openTab(tabName) {
     for(tablink of tabLinks) {
@@ -10,4 +11,12 @@ function openTab(tabName) {
     }
     event.currentTarget.classList.add('active-link');
     document.getElementById(tabName).classList.add('active-tab');
+}
+
+function openMenu() {
+    sideMenu.style.right = '0';
+}
+
+function closeMenu() {
+    sideMenu.style.right = '-200px';
 }
